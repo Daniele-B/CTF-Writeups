83a294add32b81b311e9897b098c061ce05f3767
Daniele Bianchin <bbianchind@gmail.com>
2018-10-14 14:33:32 +0200

    Update fleggo_extractor.py

--- Flare-on 5/3 - FLAGGO/fleggo_extractor.py
+++ Flare-on 5/3 - FLAGGO/fleggo_extractor.py
@@ -2,10 +2,17 @@ import os, subprocess
 
 BINARIES_PATH = 'C:\\Users\\bbian\\Desktop\\FlareOn5_Challenges\\03_FLEGGO\\FLEGGO'
 
+def shell(command):
+    try:
+        output = subprocess.check_output(command, shell=True, stderr=subprocess.STDOUT)
+    except Exception, e:
+        output = str(e.output)
+    return output.split('\n')
+
 def get_pswd(binary):
     pswd = ''
-    bina = open(BINARIES_PATH + '\\' + binary, 'rb').read()
-    i = 0x2ab0
+    bina = open(binary, 'rb').read()
+    i = 0x2ab0 #password fixed position
     while bina[i] != chr(0):
         pswd += bina[i]
         i += 2
@@ -14,6 +21,6 @@ def get_pswd(binary):
 binaries = os.listdir(BINARIES_PATH)
 
 for pe in binaries:
-    pswd = get_pswd(pe)
-    out = subprocess.check_output('echo %s | %s' % (pswd, BINARIES_PATH + '\\' + pe), shell=True).split('\n')
-    print out[1]
+    pe = BINARIES_PATH + '\\' + pe
+    out = shell('echo %s | %s' % (get_pswd(pe), pe))
+    print out[2]
